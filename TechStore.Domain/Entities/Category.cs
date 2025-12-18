d792a2688c3c02499062a67e748820911cc20b54
Tequelopor <i202412703@cibertec.edu.pe>
2025-12-18 17:10:41 -0500

    Configuracion Inicial: Entidades, DBContext y Migración SQL Server

---
+++ TechStore.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace TechStore.Domain.Entities
+{
+    [Table("categoria")] 
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+
+        public virtual ICollection<Product> Productos { get; set; } = new List<Product>();
+    }
+}
---
+++ TechStore.Domain/Entities/SaleDetail.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechStore.Domain.Entities
+{
+    [Table("detalle_venta")]
+    public class SaleDetail
+    {
+        public int Id { get; set; }
+
+        [Column("venta_id")]
+        public int VentaId { get; set; }
+
+        [Column("producto_id")]
+        public int ProductoId { get; set; }
+
+        public int Cantidad { get; set; }
+
+        [Column("precio_unitario", TypeName = "decimal(10,2)")]
+        public decimal PrecioUnitario { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Subtotal { get; set; }
+
+        // Relaciones
+        public virtual Sale? Venta { get; set; }
+        public virtual Product? Producto { get; set; }
+
+    }
+}
